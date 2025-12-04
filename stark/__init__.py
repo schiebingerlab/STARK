54432873994328d71bc303aec4031d9da539023e
Matthieu Heitz <heitz.matthieu@gmail.com>
2025-12-04 11:34:42 -0800

    Add package config files

---
+++ stark/__init__.py
@@ -0,0 +1,10 @@
+# from .stark import test
+
+# Define version here
+__version__ = '0.1.0'
+
+# Command reminder for building the package
+# Need: pip install build twine
+# For upload with twine, username and passord are already stored in ~/.pypirc
+# python3 -m build
+# python3 -m twine upload --skip-existing dist/*
